f09f272cc4b11224460cac94694d5de8c7cf554c
Abstract <xavier04.martinez@gmail.com>
2023-01-09 05:05:59 -0600

    feat: latest changes

--- src/main/java/dev/aabstractt/pvpup/AbstractPlugin.java
+++ src/main/java/dev/aabstractt/pvpup/AbstractPlugin.java
@@ -5,6 +5,7 @@ import dev.aabstractt.pvpup.factory.PerkFactory;
 import dev.aabstractt.pvpup.listener.PlayerDeathListener;
 import dev.aabstractt.pvpup.listener.PlayerJoinListener;
 import dev.aabstractt.pvpup.listener.PlayerQuitListener;
+import dev.aabstractt.pvpup.utils.visualise.WallBorderListener;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -22,5 +23,7 @@ public class AbstractPlugin extends JavaPlugin {
         this.getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
         this.getServer().getPluginManager().registerEvents(new PlayerDeathListener(), this);
         this.getServer().getPluginManager().registerEvents(new PlayerQuitListener(), this);
+
+        this.getServer().getPluginManager().registerEvents(new WallBorderListener(), this);
     }
 }
